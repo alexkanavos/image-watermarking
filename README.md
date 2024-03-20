b0e245c2c77a2b6a03a34e10a8a94cebef21bc68
Alex Kanavos <alexkanv@gmail.com>
2024-03-20 23:50:52 +0100

    Test ttkbootstrap

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Image Watermarking Desktop App
+
+This is a simple desktop application for adding watermarks to images.
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import tkinter as tk
-from tkinter import ttk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 
 
-class App(tk.Tk):
+class App(ttk.Window):
 
     def __init__(self, title: str, size: tuple):
 
@@ -23,45 +23,32 @@ class Menu(ttk.Frame):
 
     def __init__(self, parent):
         super().__init__(parent)
-        self.place(x=0, y=0, relwidth=0.3, relheight=1)
+        self.place(x=0, y=0, relwidth=1, height=40)
 
         self.create_widgets()
 
     def create_widgets(self):
 
         # create the widgets
-        menu_button_1 = ttk.Button(self, text="Button 1")
-        menu_button_2 = ttk.Button(self, text="Button 2")
-        menu_button_3 = ttk.Button(self, text="Button 3")
-
-        menu_slider_1 = ttk.Scale(self, orient="vertical")
-        menu_slider_2 = ttk.Scale(self, orient="vertical")
-
-        toggle_frame = ttk.Frame(self)
-        menu_toggle_1 = ttk.Checkbutton(toggle_frame, text="check 1")
-        menu_toggle_2 = ttk.Checkbutton(toggle_frame, text="check 2")
-
-        entry = ttk.Entry(self)
+        self.menu_button_1 = ttk.Button(
+            self, text="Close App", bootstyle=(DANGER, OUTLINE)
+        )
+        self.menu_button_2 = ttk.Button(
+            self, text="Add Image", bootstyle=(PRIMARY, OUTLINE)
+        )
+        self.menu_button_3 = ttk.Button(
+            self, text="Next Step", bootstyle=(INFO, OUTLINE)
+        )
 
         # create the grid
-        self.columnconfigure((0, 1, 2), weight=1, uniform="a")
-        self.rowconfigure((0, 1, 2, 3, 4), weight=1, uniform="a")
+
+        self.columnconfigure((0, 1, 2, 3, 4), weight=1, uniform="col")
+        self.rowconfigure((0), weight=1, uniform="row")
 
         # place the widgets
-        menu_button_1.grid(row=0, column=0, sticky="nswe", columnspan=2)
-        menu_button_2.grid(row=0, column=2, sticky="nswe")
-        menu_button_3.grid(row=1, column=0, columnspan=3, sticky="nsew")
-
-        menu_slider_1.grid(row=2, column=0, rowspan=2, sticky="nsew", pady=20)
-        menu_slider_2.grid(row=2, column=2, rowspan=2, sticky="nsew", pady=20)
-
-        # toggle layout
-        toggle_frame.grid(row=4, column=0, columnspan=3, sticky="nsew")
-        menu_toggle_1.pack(side="left", expand=True)
-        menu_toggle_2.pack(side="left", expand=True)
-
-        # entry layout
-        entry.place(relx=0.5, rely=0.95, relwidth=0.9, anchor="center")
+        self.menu_button_1.grid(row=0, column=0, padx=7, sticky="w")
+        self.menu_button_2.grid(row=0, column=2)
+        self.menu_button_3.grid(row=0, column=4, padx=7, sticky="e")
 
 
 App(title="My App", size=(600, 400))
