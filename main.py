7d86af42bd9fb85838e23f9cf65dcf279bb63388
Alex Kanavos <alexkanv@gmail.com>
2024-03-21 23:12:04 +0100

    Setup App class

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import ttkbootstrap as ttk
-from ttkbootstrap.constants import *
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
 
 
-class App(ttk.Window):
+class App(tk.Tk):
 
     def __init__(self, title: str, size: tuple):
 
@@ -12,43 +13,60 @@ class App(ttk.Window):
         self.geometry(f"{size[0]}x{size[1]}")
         self.minsize(size[0], size[1])
 
+        self.columnconfigure((0, 1), weight=1, uniform="a")
+        self.rowconfigure(0, weight=1)
+
         # widgets
         self.menu = Menu(self)
+        self.menu.grid(row=0, column=1)
+        self.image_area = ImageArea(self)
+        self.image_area.grid(row=0, column=0)
 
         # run
         self.mainloop()
 
 
 class Menu(ttk.Frame):
-
     def __init__(self, parent):
         super().__init__(parent)
-        self.place(x=0, y=0, relwidth=1, height=40)
+        self.place(relx=0.75, y=40, relwidth=1, relheight=1)
 
         self.create_widgets()
 
     def create_widgets(self):
 
-        # create the widgets
-        self.menu_button_1 = ttk.Button(
-            self, text="Close App", bootstyle=(DANGER, OUTLINE)
-        )
-        self.menu_button_2 = ttk.Button(
-            self, text="Add Image", bootstyle=(PRIMARY, OUTLINE)
-        )
-        self.menu_button_3 = ttk.Button(
-            self, text="Next Step", bootstyle=(INFO, OUTLINE)
-        )
+        self.text_label = ttk.Label(self, text="Watermark")
+        self.text_entry = ttk.Entry(self)
+
+        self.font_family_label = ttk.Label(self, text="Font Family")
+        self.font_family_entry = ttk.Entry(self)
+
+        self.font_size_label = ttk.Label(self, text="Font Size")
+        self.font_size_entry = ttk.Entry(self)
 
         # create the grid
-
-        self.columnconfigure((0, 1, 2, 3, 4), weight=1, uniform="col")
-        self.rowconfigure((0), weight=1, uniform="row")
+        self.columnconfigure((0), weight=1, uniform="col")
+        self.rowconfigure((0, 1, 2, 3, 4, 5), weight=1, uniform="row")
 
         # place the widgets
-        self.menu_button_1.grid(row=0, column=0, padx=7, sticky="w")
-        self.menu_button_2.grid(row=0, column=2)
-        self.menu_button_3.grid(row=0, column=4, padx=7, sticky="e")
+        self.text_label.grid(row=0, column=0, sticky="ws")
+        self.text_entry.grid(row=1, column=0, sticky="wn")
+
+        self.font_family_label.grid(row=2, column=0, sticky="ws")
+        self.font_family_entry.grid(row=3, column=0, sticky="wn")
+
+        self.font_size_label.grid(row=4, column=0, sticky="ws")
+        self.font_size_entry.grid(row=5, column=0, sticky="wn")
 
 
-App(title="My App", size=(600, 400))
+class ImageArea(tk.Canvas):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    # def import_image(self):
+    #     self.image_original = Image.open("lofico_study_beach.jpg")
+    #     self.image_ratio = self.image_original.size[0] / self.image_original.size[1]
+    #     self.image_tk = ImageTk.PhotoImage(self.image_original)
+
+
+App(title="My App", size=(800, 600))
