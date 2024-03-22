2d579d4debe08369c42e6b8ac637c2e11c84c894
Alex Kanavos <alexkanv@gmail.com>
2024-03-22 15:49:19 +0100

    Add image inside canvas

--- main.py
+++ main.py
@@ -7,66 +7,67 @@ class App(tk.Tk):
 
     def __init__(self, title: str, size: tuple):
 
-        # main setup
+        # window setup
         super().__init__()
         self.title(title)
         self.geometry(f"{size[0]}x{size[1]}")
-        self.minsize(size[0], size[1])
-
-        self.columnconfigure((0, 1), weight=1, uniform="a")
+        self.columnconfigure((0, 1, 2, 3), weight=1, uniform="a")
         self.rowconfigure(0, weight=1)
 
-        # widgets
+        # window widgets and their positions on grid
         self.menu = Menu(self)
         self.menu.grid(row=0, column=1)
+
         self.image_area = ImageArea(self)
-        self.image_area.grid(row=0, column=0)
+        self.image_area.grid(column=1, columnspan=3, row=0, sticky="nsew")
 
         # run
         self.mainloop()
 
 
 class Menu(ttk.Frame):
+
     def __init__(self, parent):
         super().__init__(parent)
-        self.place(relx=0.75, y=40, relwidth=1, relheight=1)
 
         self.create_widgets()
 
     def create_widgets(self):
-
-        self.text_label = ttk.Label(self, text="Watermark")
-        self.text_entry = ttk.Entry(self)
-
-        self.font_family_label = ttk.Label(self, text="Font Family")
-        self.font_family_entry = ttk.Entry(self)
-
-        self.font_size_label = ttk.Label(self, text="Font Size")
-        self.font_size_entry = ttk.Entry(self)
-
-        # create the grid
-        self.columnconfigure((0), weight=1, uniform="col")
-        self.rowconfigure((0, 1, 2, 3, 4, 5), weight=1, uniform="row")
-
-        # place the widgets
-        self.text_label.grid(row=0, column=0, sticky="ws")
-        self.text_entry.grid(row=1, column=0, sticky="wn")
-
-        self.font_family_label.grid(row=2, column=0, sticky="ws")
-        self.font_family_entry.grid(row=3, column=0, sticky="wn")
-
-        self.font_size_label.grid(row=4, column=0, sticky="ws")
-        self.font_size_entry.grid(row=5, column=0, sticky="wn")
+        pass
 
 
 class ImageArea(tk.Canvas):
     def __init__(self, parent):
         super().__init__(parent)
+        self.configure(background="black", bd=0, highlightthickness=0, relief="ridge")
 
-    # def import_image(self):
-    #     self.image_original = Image.open("lofico_study_beach.jpg")
-    #     self.image_ratio = self.image_original.size[0] / self.image_original.size[1]
-    #     self.image_tk = ImageTk.PhotoImage(self.image_original)
+        # to be changed --> button upload
+        self.image_original = Image.open("lofico_study_beach.jpg")
+        self.image_ratio = self.image_original.size[0] / self.image_original.size[1]
+        self.image_tk = ImageTk.PhotoImage(self.image_original)
+
+        self.bind("<Configure>", self.show_full_image)
+
+    def show_full_image(self, event):
+
+        # current canvas ratio
+        self.canvas_ratio = event.width / event.height
+
+        if self.canvas_ratio > self.image_ratio:
+            self.height = int(event.height)
+            self.width = int(self.height * self.image_ratio)
+        else:
+            self.width = int(event.width)
+            self.height = int(self.width / self.image_ratio)
+
+        self.resized_image = self.image_original.resize((self.width, self.height))
+        self.resized_tk = ImageTk.PhotoImage(self.resized_image)
+        self.create_image(
+            int(event.width / 2),
+            int(event.height / 2),
+            anchor="center",
+            image=self.resized_tk,
+        )
 
 
 App(title="My App", size=(800, 600))
